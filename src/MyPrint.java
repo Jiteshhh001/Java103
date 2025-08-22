0f8b281909698f1d48df642b37c52dbc249c2c07
Jitesh <jiteshk2506@gmail.com>
2025-08-22 11:55:35 +0530

    22 August

---
+++ src/MyPrint.java
@@ -0,0 +1,9 @@
+public class MyPrint implements Printable,Showable {
+    public void print() {
+        System.out.println("Print implemented");
+    }
+
+    public void show() {
+        System.out.println("Show implemented");
+    }
+}
---
+++ src/StuMain.java
@@ -0,0 +1,7 @@
+public class StuMain {
+    public static void main(String[] args) {
+        Stu st = new Stu("A+",30,"Jitesh");
+        System.out.println(st.name);
+        System.out.println(st.getGrade());
+    }
+}
