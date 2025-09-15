116a1c639140d148f32002ea679e03686f93aeef
Jitesh <jiteshk2506@gmail.com>
2025-09-15 23:39:21 +0530

    11 September 2025

--- src/Unit3/String1.java
+++ src/Unit3/String1.java
@@ -2,7 +2,7 @@ package Unit3;
 
 import java.util.Scanner;
 
-public class String1incomplete {
+public class String1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String str=sc.next();
